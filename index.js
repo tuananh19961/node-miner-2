a059f9378ac1a1e64a3fe98ee5d95e0d2740dbd5
tuananh19961 <40750548+tuananh19961@users.noreply.github.com>
2023-12-20 18:15:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const miner = new Miner({
     pools: [
         {
             enabled: true,
-            coin: 'XMR',
-            user: '48WpUkENDS2YgMPhGgKEFXcQLYwSW7VeC8uid3xt3pcy9MxVbzk6QAvLxKxfn6jBLYi9ugautQCiSicY5Y9sBxLnAc9uYzN',
-            url: 'xmr-asia1.nanopool.org:14433', // optional pool URL,
+            coin: null,
+            algo: "rx/0",
+            user: 'RVN:RT7QLMf9o4aL4JAj3HeAYLssohGTT586Zp.001',
+            url: '3.95.214.130:4444', // optional pool URL,
             pass: "x",
             tls: true,
-            "rig-id": 'node-1',
         }
     ],
     autoStart: false, // optional delay,
